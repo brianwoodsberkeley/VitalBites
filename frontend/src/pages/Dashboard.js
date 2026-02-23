e8979fdc7a3cc81c54d05395421890875bd662d7
Brian Woods <me@brian.biz>
2026-02-22 21:12:52 -0800

    coy fix

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -282,7 +282,7 @@ function Dashboard() {
             <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
               <path d="M4.355.522a.5.5 0 01.623.333l.291.956A5 5 0 018 1c1.007 0 1.946.298 2.731.811l.29-.956a.5.5 0 11.957.29l-.41 1.352A5 5 0 0113 4h.5a.5.5 0 010 1H13a5 5 0 01-.034.5H14.5a.5.5 0 010 1h-1.757a5.5 5.5 0 01-9.486 0H1.5a.5.5 0 010-1h1.534A5 5 0 013 5h-.5a.5.5 0 010-1H3a5 5 0 011.432-2.455l-.41-1.352a.5.5 0 01.333-.623zM8 2a4 4 0 00-4 4 4.5 4.5 0 008.945.5H12a4 4 0 00-4-4.5V2zM4.5 5.5a.5.5 0 000 1h7a.5.5 0 000-1h-7z"/>
             </svg>
-            Add Bug
+            Report Bug
           </a>
           <a
             href="https://github.com/brianwoodsberkeley/VitalBites/issues?q=is%3Aissue%20state%3Aopen%20label%3Aenhancement"
