30e61493755d619e4469e8eb29aa3af0af1ca395
Brian Woods <me@brian.biz>
2026-04-06 18:41:23 -0700

    fixes to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh -i VitalStaff.pem ec2-user@54.81.134.8 'cd VitalBites; git pull; cd frontend; npm run build'
+ssh -i VitalStaff.pem ec2-user@54.81.134.8 'cd VitalBites; git pull; cd frontend;REACT_APP_API_URL=https://www.vitalfoods.ai/api npm run build'
