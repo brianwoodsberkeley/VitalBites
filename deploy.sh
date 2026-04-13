440aeb789b7b3bd49a754bdabfc598120d4bb607
Brian Woods <me@brian.biz>
2026-04-12 17:23:35 -0700

    fixes for install

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh -i VitalStaff.pem ec2-user@54.81.134.8 'cd VitalBites; git pull; cd frontend;npm run build'
+ssh -i VitalStaff.pem ec2-user@54.81.134.8 'set -e; cd VitalBites; git pull; ./venv/bin/pip install -r backend/requirements.txt; cd frontend; npm run build; cd ..; sudo systemctl restart vitalbites'
